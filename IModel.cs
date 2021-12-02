c7172931ff79aab9ee6315706603883a5c6abaae
Mikhail Galin <mishagalin85@gmail.com>
2021-12-02 19:23:55 +0500

    Часть 1, задание 2

---
+++ IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Sem3_Galin_Mihail
+{
+    internal interface IModel
+    {
+        void AddNode(int value);
+
+        void RemoveLastNode();
+
+        int Count { get; }
+    }
+}
