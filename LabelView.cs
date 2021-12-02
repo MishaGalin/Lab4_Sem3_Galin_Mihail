9a8847bfb5f68810b19175aaa5bb6f2a345052c5
Mikhail Galin <mishagalin85@gmail.com>
2021-12-02 19:54:52 +0500

    Часть 1, задание 7

---
+++ LabelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_Sem3_Galin_Mihail
+{
+    internal class LabelView : IView
+    {
+        private IModel model;
+        private Label l;
+        public IModel Model { get => model; set => model = value; }
+
+        public LabelView(Label l)
+        {
+            this.l = l;
+        }
+
+        public void UpdateView() => l.Text = model.Count.ToString();
+    }
+}
