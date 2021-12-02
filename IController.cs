8670c6b3077cb3f9cb9c2b50a9ff98a8063a9e0f
Mikhail Galin <mishagalin85@gmail.com>
2021-12-02 19:26:38 +0500

    Часть 1, задание 4

---
+++ IController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Sem3_Galin_Mihail
+{
+    internal interface IController
+    {
+        IModel Model { get; set; }
+
+        void AddView(IView v);
+
+        void Add();
+
+        void Remove();
+    }
+}
