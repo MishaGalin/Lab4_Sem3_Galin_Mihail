bf59d94708ac366788da10e40e0e5ef93da39429
Mikhail Galin <mishagalin85@gmail.com>
2021-12-02 18:52:45 +0500

    Добавьте файлы проекта.

---
+++ Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Sem3_Galin_Mihail
+{
+    internal class Node
+    {
+        private int x, y, value;
+
+        public Node(int value, int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = value;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Value { get => value; set => this.value = value; }
+    }
+}
